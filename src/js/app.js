9f09a2e407a22c8ef4801756dfa75bfae8a13fd9
Volkan Ongun <me@volkanongun.com>
2020-02-04 16:42:53 +0300

    React added

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,13 @@
 import name from "./multipleFiles";
+import ReactDOM from 'react-dom';
+import React from 'react';
 
 console.log("hello console", name);
+
+function App(){
+	return <div>
+		<h2>I'm React</h2>
+	</div>
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
